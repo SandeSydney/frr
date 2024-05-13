7eac002f9890baf6f5ebf6e70eb982e310123fdc
SandeSydney <sandesydney01@gmail.com>
2024-05-13 18:20:07 +0300

    Added Navigation Bar for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import NavigationBar from './components/navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <NavigationBar/>
   );
 }
 
