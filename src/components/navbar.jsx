02639a0a566dd58ae947b940404bcc4a23d53e64
SandeSydney <sandesydney01@gmail.com>
2024-05-13 18:08:21 +0300

    Added basic navigation bar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container> 
+        <Navbar.Brand href="">Fred's Ranch & Resort</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto">
+          <NavDropdown title="Restaurant" id="basic-nav-dropdown">
+              <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="">
+                Events & Activities
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="">Agritourism</Nav.Link>
+            <Nav.Link href="">School</Nav.Link>
+            <Nav.Link href="">Contact Us</Nav.Link>
+            <Nav.Link href="">Blog</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  );
+}
+
+export default BasicExample;
