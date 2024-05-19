231d8b20353844e995653a94c3147ee11fa1278b
SandeSydney <sandesydney01@gmail.com>
2024-05-19 18:22:41 +0300

    Imported and implemented carousel

--- src/App.js
+++ src/App.js
@@ -4,37 +4,43 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./App.css";
 import frr_logo from "./resources/frr-logo.png";
+import MainCarousel from "./components/carousel";
 
 function App() {
   return (
     <>
-      <Navbar expand="md" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              className="w-28 md:w-40"
-              src={frr_logo}
-              alt="Fred's Ranch and Resort Logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="">
-              <NavDropdown title="Restaurant" id="basic-nav-dropdown">
-                <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="">Events & Activities</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="">Agritourism</Nav.Link>
-              <Nav.Link href="">School</Nav.Link>
-              <Nav.Link href="">Contact Us</Nav.Link>
-              <Nav.Link href="">Blog</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar expand="md" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                className="w-28 md:w-40"
+                src={frr_logo}
+                alt="Fred's Ranch and Resort Logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="">
+                <NavDropdown title="Restaurant" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="">
+                    Events & Activities
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="">Agritourism</Nav.Link>
+                <Nav.Link href="">School</Nav.Link>
+                <Nav.Link href="">Contact Us</Nav.Link>
+                <Nav.Link href="">Blog</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className="flex justify-center">
+          <MainCarousel />
+        </div>
     </>
   );
 }
