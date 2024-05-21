c2f432083a6e756809675a60b15d534ee93006e2
SandeSydney <sandesydney01@gmail.com>
2024-05-21 09:14:57 +0300

    Removed navbar to separate component.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "./App.css";
-import frr_logo from "./resources/frr-logo.png";
 import MainCarousel from "./components/carousel";
+import Navbar_Main from "./components/navbar";
 
 function App() {
   return (
     <>
-        <Navbar expand="md" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="/">
-              <img
-                className="w-28 md:w-40"
-                src={frr_logo}
-                alt="Fred's Ranch and Resort Logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="">
-                <NavDropdown title="Restaurant" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="">
-                    Events & Activities
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="">Agritourism</Nav.Link>
-                <Nav.Link href="">School</Nav.Link>
-                <Nav.Link href="">Contact Us</Nav.Link>
-                <Nav.Link href="">Blog</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Navbar_Main/>
         <div className="flex justify-center">
           <MainCarousel />
         </div>
