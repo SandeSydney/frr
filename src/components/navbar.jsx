94f0c8d4700dd244231e4e5fb2f623a4eff3b0d0
SandeSydney <sandesydney01@gmail.com>
2024-05-25 20:02:23 +0300

    Implemented navbar display on scroll function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,35 +3,53 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import frr_logo from "../resources/frr-logo.png";
+import { useState } from "react";
 
 export default function Navbar_Main() {
+  // setting constant to determine if the navigation bar will be visible
+  const [visible, setVisible] = useState(false);
+
+  // function to toggle the navbar visibility by scrolling past a certain height.
+  function toggleNavigation() {
+    if (window.scrollY >= 50) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+
+  // Adding an event listener for the window to monitor scrolling, calling the toggle navigation function
+  window.addEventListener("scroll", toggleNavigation);
+
   return (
-    <Navbar expand="md" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            className="w-28 md:w-40"
-            src={frr_logo}
-            alt="Fred's Ranch and Resort Logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="">
-            <NavDropdown title="Restaurant" id="basic-nav-dropdown">
-              <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="">Events & Activities</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="">Agritourism</Nav.Link>
-            <Nav.Link href="">School</Nav.Link>
-            <Nav.Link href="">Contact Us</Nav.Link>
-            <Nav.Link href="">Blog</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar expand="md" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              className="w-28 md:w-40"
+              src={frr_logo}
+              alt="Fred's Ranch and Resort Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="">
+              <NavDropdown title="Restaurant" id="basic-nav-dropdown">
+                <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">Events & Activities</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="">Agritourism</Nav.Link>
+              <Nav.Link href="">School</Nav.Link>
+              <Nav.Link href="">Contact Us</Nav.Link>
+              <Nav.Link href="">Blog</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
