5d3a8539825ce0d85fb50b0a128d53ca5f63d8e4
SandeSydney <sandesydney01@gmail.com>
2024-05-26 16:04:39 +0300

    Styled with a div

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+import About from "./components/about";
 import MainCarousel from "./components/carousel";
 import Navbar_Main from "./components/navbar";
 
 function App() {
   return (
-    <>
-        <Navbar_Main/>
-        <div className="flex justify-center">
-          <MainCarousel />
-        </div>
-    </>
+    <div className="flex flex-col gap-4">
+      <Navbar_Main />
+      <div className="flex justify-center">
+        <MainCarousel />
+      </div>
+      <About />
+    </div>
   );
 }
 
