7b8806a477a77d270190a75065f7bc347eaa168d
SandeSydney <sandesydney01@gmail.com>
2024-05-26 15:22:11 +0300

    Added the about section

---
+++ src/components/about.jsx
@@ -0,0 +1,34 @@
+export default function About() {
+  return (
+    <section className="flex text-center w-full justify-center content-around">
+      <div className="w-5/6 flex flex-col gap-2">
+        <h1 className="text-center">About Us</h1>
+        <p className="main_text">
+          Fred's Ranch & Resort is an integral part of Kajiado Getaways, setting
+          the standards for excellence in vacation experiences. It is located 70
+          kilometers from Nairobi City Center, built on a 10 acre farm with a
+          spread of Cowboy theme against the natural background of beautiful
+          lawns, haystacks, dairy cattle and smiling faces.
+        </p>
+        <p className="main_text">
+          Most importantly, we love our food! Chefs at Freds Ranch & Resort
+          serve orginically prepared dishes, fresh from the farm, and designed
+          to titillate your palate with the delectable Kenyan Cuisine of tender
+          meats from the grill and traditional vegetables from the farm. Fred's
+          Ranch & Resort is a great choice if you are looking for the best Nyama
+          Choma places in Kitengela.
+        </p>
+        <p className="main_text">
+          The easy Rhumba Music playing in the background as you enjoy your
+          favourite cocktail or having a dip in the exquisite swimming pool is
+          definitely the perfect escape from the hustle of the city. Truly,
+          Fred's Ranch & Resort is the best place to visit in Kitengela.
+        </p>
+        <p className="main_text">
+          You have to be at Fred's Ranch & Resort to understand and experience
+          the Vibe!
+        </p>
+      </div>
+    </section>
+  );
+}
