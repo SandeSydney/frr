21e9d93dca6903d348a17e9fa8eb9ec286db6cfe
SandeSydney <sandesydney01@gmail.com>
2024-06-08 17:05:51 +0300

    Created Landing page component

---
+++ src/components/landing_page.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from "react-type-animation";
+import arrow_down from "../resources/arrow_down.png";
+
+const Landing = () => {
+  return (
+    <>
+      <TypeAnimation
+        className="landing_text"
+        sequence={["Welcome!!!", 500, "-FRED'S RANCH & RESORT-"]}
+        speed={70}
+        cursor={false}
+      />
+      <TypeAnimation
+        className="landing_subtext"
+        sequence={[3000, "Feels Just Like Grandma's..."]}
+        speed={60}
+        cursor={false}
+        preRenderFirstString={true}
+      />
+      <div className="flex h-7">
+        <p className="text-white text-sm underline underline-offset-8">
+          Scroll Down <span></span>
+        </p>
+        <div>
+          <img width="30px" src={arrow_down} alt="Arrow Down" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
