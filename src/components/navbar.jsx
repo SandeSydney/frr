aa83234d9c14feb33545879b7b3261af4bbada54
SandeSydney <sandesydney01@gmail.com>
2024-06-08 18:53:38 +0300

    Removed the Nav section to a different component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import frr_logo from "../resources/frr-logo.png";
 import { useState } from "react";
+import Nav_Section from "./nav_section";
 
 export default function Navbar_Main() {
   // setting constant to determine if the navigation bar will be visible
@@ -11,7 +12,7 @@ export default function Navbar_Main() {
 
   // function to toggle the navbar visibility by scrolling past a certain height.
   function toggleNavigation() {
-    if (window.scrollY >= 200) {
+    if (window.scrollY >= 300) {
       setVisible(true);
     } else {
       setVisible(false);
@@ -22,32 +23,8 @@ export default function Navbar_Main() {
   window.addEventListener("scroll", toggleNavigation);
 
   return (
-      <Navbar expand="md" className={visible?"navbar_open":"navbar_closed"}>
-        <Container className="uppercase">
-          <Navbar.Brand href="/">
-            <img
-              className="w-28 md:w-40"
-              src={frr_logo}
-              alt="Fred's Ranch and Resort Logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="">
-              <NavDropdown title="Restaurant" id="basic-nav-dropdown">
-                <NavDropdown.Item href="">Food Menu</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="">Events & Activities</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="">Accomodation</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="">Agritourism</Nav.Link>
-              <Nav.Link href="">School</Nav.Link>
-              <Nav.Link href="">Contact Us</Nav.Link>
-              <Nav.Link href="">Blog</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Navbar expand="md" className={visible ? "navbar_open" : ""}>
+      <Nav_Section />
+    </Navbar>
   );
 }
