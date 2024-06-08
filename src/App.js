6d50781d060422c1b76a39e703d5c5e4c1ba709b
SandeSydney <sandesydney01@gmail.com>
2024-06-08 18:51:41 +0300

    Added Secondary Navigation Bar and state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import About from "./components/about";
 import MainCarousel from "./components/carousel";
@@ -6,16 +7,26 @@ import Landing from "./components/landing_page";
 import Navbar_Main from "./components/navbar";
 import Numbers from "./components/numbers";
 import Team from "./components/team";
+import Navbar_Sec from "./components/navbar_sec";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex flex-col gap-5">
+      {window.addEventListener("scroll", () => {
+        if (window.scrollY >= 300) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      })}
+      {visible ? <Navbar_Main /> : <Navbar_Sec />}
       <div className="home">
         <div className="landing">
           <Landing />
         </div>
       </div>
-      <Navbar_Main />
+
       <About />
     </div>
   );
