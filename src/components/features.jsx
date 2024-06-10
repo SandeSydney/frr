e46a428890a3241ae85fbf926f346eae1f9134c3
SandeSydney <sandesydney01@gmail.com>
2024-06-10 09:00:35 +0300

    Added Features component

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,9 +1,17 @@
 import Container from "react-bootstrap/Container";
+import sunset_pool from "../resources/sunset_pool.jpg";
 
 export default function Features() {
   return (
-    <Container>
-      Features
-    </Container>
+    <div className="features">
+      <Container className="features_container">
+        <img src={sunset_pool} alt="feature image" />
+
+        <div>
+          <h2>Main View Of The Ranch</h2>
+        </div>
+        <div>images & text</div>
+      </Container>
+    </div>
   );
 }
