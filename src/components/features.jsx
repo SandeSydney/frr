c8e86d3b41856f7752cf0849b644ad20e547ef0d
SandeSydney <sandesydney01@gmail.com>
2024-06-10 11:57:03 +0300

    Added text describing the ranch

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,5 +1,7 @@
 import Container from "react-bootstrap/Container";
 import sunset_pool from "../resources/sunset_pool.jpg";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Features() {
   return (
@@ -7,10 +9,28 @@ export default function Features() {
       <Container className="features_container">
         <img src={sunset_pool} alt="feature image" />
 
-        <div>
-          <h2>Main View Of The Ranch</h2>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-center font-extrabold font-[Dancing-Script]">
+            Glimpse Of The Nature In The Ranch
+          </h2>
+          <div className="features_glimpse">
+            <Row>
+              <Col sm={12} md={6}>
+                <p className="font-[Dancing-Script] tracking-wider lg:text-xl">
+                  Fred's Ranch & Resort is an integral part of Kajiado Getaways,
+                  and located 70 kilometers from Nairobi City Center, we set the
+                  standards for excellence in vacation experiences.The easy
+                  Rhumba Music playing in the background as you enjoy your
+                  favourite cocktail or having a dip in the exquisite swimming
+                  pool is definitely the perfect escape from the hustle of the
+                  city.
+                </p>
+                <img src="" alt="" />
+              </Col>
+              <Col sm={12} md={6}></Col>
+            </Row>
+          </div>
         </div>
-        <div>images & text</div>
       </Container>
     </div>
   );
