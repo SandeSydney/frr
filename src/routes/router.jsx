506e47cc00265c8e9218caad4cd81dc44c2f9bba
SandeSydney <sandesydney01@gmail.com>
2024-06-15 15:56:25 +0300

    Added about, events, home, dining pages to route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Error_Page from "../components/error_page";
+import Homepage from "../components/homepage";
+import AboutPage from "../components/aboutpage";
+import DiningPage from "../components/diningpage";
+import EventsPage from "../components/eventspage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <Error_Page />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "dining",
+        element: <DiningPage />,
+      },
+      {
+        path: "events",
+        element: <EventsPage />,
+      },
+    ],
   },
 ]);
 
