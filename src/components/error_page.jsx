7e8e0e3e24f098a66e0e727accf42ef5db5c32ce
SandeSydney <sandesydney01@gmail.com>
2024-06-15 14:49:21 +0300

    Customized error page

--- src/components/error_page.jsx
+++ src/components/error_page.jsx
@@ -1,5 +1,20 @@
-export default function Error_Page(){
-    return(
-        <div>Error</div>
-    );
+import { useRouteError } from "react-router-dom";
+import Navbar_Sec from "./navbar_sec";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <>
+      <Navbar_Sec />
+      <div id="error_page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>Page Requested Is {error.statusText || error.message}!</i>
+        </p>
+      </div>
+    </>
+  );
 }
