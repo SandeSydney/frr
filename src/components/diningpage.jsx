7470fc4489336ad3d71fc85d39341856b538bd65
SandeSydney <sandesydney01@gmail.com>
2024-06-18 17:07:17 +0300

    Modified custom dining page

--- src/components/diningpage.jsx
+++ src/components/diningpage.jsx
@@ -1,63 +1,22 @@
-export default function DiningPage(){
-    return(
-        <div>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
-            <p>Dining</p>
+import Container from "react-bootstrap/Container";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+
+export default function DiningPage() {
+  return (
+    <>
+      <div className="dining_landing">
+        <div className="dining_main">
+          <h1 className="font-bold">- DINE WITH US -</h1>
+          <p className="text-white text-xl md:text-2xl font-semibold italic">
+            ... If you haven't had our nyama choma then you haven't had nyama
+            choma yet{" "}
+            <span>
+              <FormatQuoteIcon className="text-[#ffcf62]" />
+            </span>
+          </p>
         </div>
-    );
+      </div>
+      <Container></Container>
+    </>
+  );
 }
