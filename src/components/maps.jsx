e74bb864629299a4e861f19d2454879cf17fe4e8
SandeSydney <sandesydney01@gmail.com>
2024-06-18 15:30:29 +0300

    Added map component

---
+++ src/components/maps.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+const MapLocation = () => {
+  return (
+    <Container>
+      <div className="py-7">
+        <h4>OUR LOCATION</h4>
+        <div className="maplocation"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default MapLocation;
