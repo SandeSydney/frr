5ee2edeb68a7c867041e4c2fa5d63881586db4d7
SandeSydney <sandesydney01@gmail.com>
2024-06-18 15:09:55 +0300

    Removed unused imports

--- src/components/follow_us.jsx
+++ src/components/follow_us.jsx
@@ -1,18 +1,10 @@
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import XIcon from "@mui/icons-material/X";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FollowUS = () => {
   return (
     <Container>
       <div className="follow_us">
-        <h3>Follow Us On:</h3>
+        <h3>Follow Us:</h3>
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
             <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
