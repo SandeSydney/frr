509654a9021dfc93e81e72596e3c4fc871b91cda
SandeSydney <sandesydney01@gmail.com>
2024-06-19 10:32:27 +0300

    Changed to Algreya Font

--- src/components/diningpage.jsx
+++ src/components/diningpage.jsx
@@ -7,7 +7,7 @@ export default function DiningPage() {
       <div className="dining_landing">
         <div className="dining_main">
           <h1 className="font-bold">- DINE WITH US -</h1>
-          <p className="text-white text-xl md:text-2xl font-semibold italic">
+          <p className="text-white text-xl md:text-2xl font-semibold font-[Alegreya SC]">
             ... If you haven't had our nyama choma then you haven't had nyama
             choma yet{" "}
             <span>
