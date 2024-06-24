a7c1fb600b0fb96fd063c7e7f2f7125bbbf1bee0
SandeSydney <sandesydney01@gmail.com>
2024-06-24 15:11:59 +0300

    Added Map iFrame

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -4,8 +4,20 @@ const MapLocation = () => {
   return (
     <Container>
       <div className="py-7">
-        <h4 style={{fontFamily:"Alegreya SC"}}>Our Location</h4>
-        <div className="maplocation"></div>
+        <h4 style={{ fontFamily: "Alegreya SC" }}>Our Location</h4>
+        <div className="maplocation">
+          <iframe
+            width="100%"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=P.O%20BOX%20192,%20Isinya+(Fred's%20Ranch%20&amp;%20Resort)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.gps.ie/">gps devices</a>
+          </iframe>
+        </div>
       </div>
     </Container>
   );
