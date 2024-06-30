b440fcd727e72b1b13ae79ed011587d6edccf8dc
SandeSydney <sandesydney01@gmail.com>
2024-06-30 16:30:54 +0300

    Added choma carousel items

--- src/components/events_carousel.jsx
+++ src/components/events_carousel.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import weddingPeople from "../resources/eventsImageGallery/weddingPeople.jpg";
 import diving from "../resources/eventsImageGallery/diving.jpg";
 import swimmingKids from "../resources/eventsImageGallery/kidsSwimming.jpg";
+import chomaFilm from "../resources/foodImageGallery/chomaFilm.jpg";
+import chomaHang from "../resources/foodImageGallery/chomaHang.jpg";
 
 function EventsCarousel() {
   return (
     <Carousel className="events_carousel">
       <Carousel.Item>
-        <img src={weddingPeople} alt="" />
-      </Carousel.Item>
-      <Carousel.Item>
         <img src={swimmingKids} alt="" />
       </Carousel.Item>
       <Carousel.Item>
         <img src={diving} alt="" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={chomaFilm} alt="" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={chomaHang} alt="" />
       </Carousel.Item>
     </Carousel>
   );
