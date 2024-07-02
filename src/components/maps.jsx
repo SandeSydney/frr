9da1bbb1c26a61b3bed99a4800807a3b6ea869b0
SandeSydney <sandesydney01@gmail.com>
2024-07-02 16:00:59 +0300

    Update on the map iframe

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -6,17 +6,20 @@ const MapLocation = () => {
       <div className="py-7">
         <h4 style={{ fontFamily: "Alegreya SC" }}>Our Location</h4>
         <div className="maplocation">
-          <iframe
-            width="100%"
-            height="600"
-            frameborder="0"
-            scrolling="no"
-            marginheight="0"
-            marginwidth="0"
-            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=P.O%20BOX%20192,%20Isinya+(Fred's%20Ranch%20&amp;%20Resort)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          >
-            <a href="https://www.gps.ie/">gps devices</a>
-          </iframe>
+            <iframe
+              style={{
+                height: "100%",
+                width: "100%",
+                border: 0,
+                frameBorder: "0",
+              }}
+              src="https://www.google.com/maps/embed/v1/place?q=Fred's+Ranch&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            ></iframe>
+          <a
+            class="googlemaps-made"
+            href="https://www.bootstrapskins.com/themes"
+            id="grab-map-info"
+          ></a>
         </div>
       </div>
     </Container>
