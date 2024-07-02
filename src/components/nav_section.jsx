2ff7d442f53e480d1368e45a91d082f93cb61cac
SandeSydney <sandesydney01@gmail.com>
2024-07-02 10:22:07 +0300

    Added links

--- src/components/nav_section.jsx
+++ src/components/nav_section.jsx
@@ -25,9 +25,9 @@ export default function Nav_Section() {
           <Link to={"about-us"}>About Us</Link>
           <Link to={"dining"}>Dining</Link>
           <Link to={"events"}>Events</Link>
-          <Link to={""}>Accommodation</Link>
+          <Link to={"accommodation"}>Accommodation</Link>
           <Link to={""}>School</Link>
-          <Link to={""}>Contact Us</Link>
+          <Link to={"contact-us"}>Contact Us</Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
