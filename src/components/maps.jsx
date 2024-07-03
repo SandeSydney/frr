169565d9f193661c687d88912af5660cc6aca550
SandeSydney <sandesydney01@gmail.com>
2024-07-03 07:45:38 +0300

    Revert "Update on the map iframe"
    This reverts commit 9da1bbb1c26a61b3bed99a4800807a3b6ea869b0.

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -24,6 +24,17 @@ const MapLocation = () => {
             href="https://www.bootstrapskins.com/themes"
             id="grab-map-info"
           ></a>
+          <iframe
+            width="100%"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=P.O%20BOX%20192,%20Isinya+(Fred's%20Ranch%20&amp;%20Resort)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.gps.ie/">gps devices</a>
+          </iframe>
         </div>
       </div>
     </Container>
