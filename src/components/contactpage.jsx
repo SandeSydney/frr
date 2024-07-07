5ee985c7e79dc5a0081ca3857d2dc82bfe68960d
SandeSydney <sandesydney01@gmail.com>
2024-07-07 12:18:13 +0300

    Added spacer on small devices

--- src/components/contactpage.jsx
+++ src/components/contactpage.jsx
@@ -5,10 +5,12 @@ import ContactForm from "./contact_form";
 function ContactUsPage() {
   return (
     <div>
+      <div className="about_spacer"></div>
       <Container>
         <div className="contact_landing">
           <h1>Get in touch</h1>
           <p>Ready to experience an adventure you will live to remember?</p>
+          <h2>LET US HEAR FROM YOU!</h2>
         </div>
       </Container>
       <div className="form_landing">
