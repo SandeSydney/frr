efbaf7d35f8650defcfad9f52d065bb5be649b54
SandeSydney <sandesydney01@gmail.com>
2024-07-10 09:28:38 +0300

    Added Wi-Fi svg

--- src/components/accommodationPage.jsx
+++ src/components/accommodationPage.jsx
@@ -38,7 +38,11 @@ function AccommodationPage() {
             <h4 className="font-bold">Accommodation Features</h4>
             <Row>
               <Col className="accom_features">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 512"
+                  color="white"
+                >
                   <path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z" />
                 </svg>
                 Bed
@@ -67,7 +71,12 @@ function AccommodationPage() {
                 </svg>
                 Breakfast
               </Col>
-              <Col className="accom_features">Wi-Fi</Col>
+              <Col className="accom_features">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                  <path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" />
+                </svg>
+                Wi-Fi
+              </Col>
             </Row>
           </Container>
         </div>
