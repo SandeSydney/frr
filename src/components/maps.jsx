bf03a3d05e8659a3f216f56d399c49f9d5f8f60c
SandeSydney <sandesydney01@gmail.com>
2024-07-15 16:32:27 +0300

    Added map from Google Business

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,9 +1,5 @@
 import { Container } from "react-bootstrap";
 
-var src =
-  "https://www.google.com/maps/embed/v1/place?q=Fred's+Ranch&key=" +
-  process.env.GOOGLE_MAP_API_KEY;
-
 const MapLocation = () => {
   return (
     <Container>
@@ -11,16 +7,13 @@ const MapLocation = () => {
         <h4 style={{ fontFamily: "Alegreya SC" }}>Our Location</h4>
         <div className="maplocation">
           <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.134663633773!2d36.871432374048155!3d-1.6645740361546304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182fb967331ddf7b%3A0xb28a05cfdd96bc35!2sFred&#39;s%20Ranch%20and%20Resort!5e0!3m2!1sen!2ske!4v1721035307148!5m2!1sen!2ske"
             width="100%"
-            height="600"
-            frameborder="0"
-            scrolling="no"
-            marginheight="0"
-            marginwidth="0"
-            src="https://www.google.com/maps/embed/v1/place?q=Fred's+Ranch&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          >
-            <a href="https://www.gps.ie/">gps devices</a>
-          </iframe>
+            height="100%"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </Container>
