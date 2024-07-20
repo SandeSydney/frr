d45ace851ec191df94e215b765b05727b3167ae5
SandeSydney <sandesydney01@gmail.com>
2024-07-20 17:09:34 +0300

    Removed quote

--- src/components/eventspage.jsx
+++ src/components/eventspage.jsx
@@ -15,13 +15,6 @@ export default function EventsPage() {
             <Col className="title_col" xs={12} sm={12} md={7}>
               <div className="flex flex-wrap gap-2">
                 <h2>Life is an Event, make it Memorable</h2>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                  height={"20px"}
-                >
-                  <path d="M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z" />
-                </svg>
               </div>
               <p>
                 Get to experience the perfect spot for all your out-door
