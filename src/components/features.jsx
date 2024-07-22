e91a91351cadb5fc2d04a264bca3823d74d77741
SandeSydney <sandesydney01@gmail.com>
2024-07-22 18:13:35 +0300

    Changed structure of the Features component

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -17,50 +17,30 @@ export default function Features() {
       <Container className="features_container">
         <img src={chomaDisplay} alt="Mbuzi choma grill" />
 
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col">
           <h2 className="text-center font-extrabold font-[Dancing-Script]">
-            A Glimpse On The Nature Of The Ranch
+            A Glimpse Of The Ranch
           </h2>
           <div className="features_glimpse">
-            <Row
-              ref={fadeRef}
-              className={inView ? "elementShow" : "elementHide"}
-            >
-              <Col sm={12} md={6}>
-                <div className="flex flex-col gap-4">
-                  <p className="font-[Dancing-Script] tracking-wider lg:text-xl">
-                    Fred's Ranch & Resort is an integral part of Kajiado
-                    Getaways, and located 70 kilometers from Nairobi City
-                    Center, we set the standards for excellence in vacation
-                    experiences.The easy Rhumba Music playing in the background
-                    as you enjoy your favourite cocktail or having a dip in the
-                    exquisite swimming pool is definitely the perfect escape
-                    from the hustle of the city.
-                  </p>
-                  <div className="flex gap-1 content-center">
-                    <Link
-                      to={"events"}
-                      className="underline underline-offset-8"
-                    >
-                      Explore Events & Activities
-                    </Link>
-                    <EastSharpIcon />
-                  </div>
-                  <img
-                    className="mb-3"
-                    src={wedding_ground}
-                    alt="wedding grounds"
-                    width=""
-                  />
-                </div>
-              </Col>
-              <Col sm={12} md={6}>
-                <div className="flex flex-col gap-4">
-                  <img src={events} alt="Events Image" />
-                  <img src={playground} alt="Playground" />
-                </div>
-              </Col>
-            </Row>
+            <div className="features_content">
+              <p className="font-[Dancing-Script] tracking-wider lg:text-xl">
+                Fred's Ranch & Resort is an integral part of Kajiado Getaways,
+                and located 70 kilometers from Nairobi City Center, we set the
+                standards for excellence in vacation experiences.The easy Rhumba
+                Music playing in the background as you enjoy your favourite
+                cocktail or having a dip in the exquisite swimming pool is
+                definitely the perfect escape from the hustle of the city.
+              </p>
+              <div className="flex gap-1 content-center">
+                <Link to={"events"} className="underline underline-offset-8">
+                  Explore Events & Activities
+                </Link>
+                <EastSharpIcon />
+              </div>
+            </div>
+            <div className="features_image">
+              <img src={events} alt="" />
+            </div>
           </div>
         </div>
       </Container>
