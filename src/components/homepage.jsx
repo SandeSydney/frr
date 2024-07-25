ae33760aace87f1d06f6078f37554fea21c0d79c
SandeSydney <sandesydney01@gmail.com>
2024-07-25 16:52:46 +0300

    Added carousel to homepage

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,12 +1,15 @@
+import { Carousel, CarouselItem } from "react-bootstrap";
 import Amenities from "./amenities";
 import Features from "./features";
 import Landing from "./landing_page";
 import Welcome_Section from "./welcome";
+import HomeCarousel from "./homeCarousel";
 
 export default function Homepage() {
   return (
     <>
       <div className="home">
+        <HomeCarousel />
         <div className="landing">
           <Landing />
         </div>
