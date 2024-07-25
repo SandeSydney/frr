ac9e5b5fcf6f511ae15e1a1bc95c50ff096a7e61
SandeSydney <sandesydney01@gmail.com>
2024-07-25 17:00:12 +0300

    Updated carousel interval

--- src/components/homeCarousel.jsx
+++ src/components/homeCarousel.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import cabbana from "../resources/mainGallery/cabbanaImage.webp";
-import horseBack from "../resources/mainGallery/horse_back.webp";
-import swimmingNight from "../resources/mainGallery/swimming_night.webp"
+import horseBack from "../resources/mainGallery/horseBackLg.webp";
+import swimmingNight from "../resources/mainGallery/swimmingNightLg.webp"
 
 function HomeCarousel() {
   return (
     <Carousel fade>
-      <Carousel.Item>
+      <Carousel.Item interval={800}>
         <img src={cabbana} alt="" />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={800}>
         <img src={horseBack} alt="" />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={800}>
         <img src={swimmingNight} alt="" />
       </Carousel.Item>
     </Carousel>
