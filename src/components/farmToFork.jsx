091ca3446cc01433a6d0464f4ce22de2864a7262
SandeSydney <sandesydney01@gmail.com>
2024-07-28 16:58:05 +0300

    Removed section

--- src/components/farmToFork.jsx
+++ src/components/farmToFork.jsx
@@ -5,13 +5,7 @@ function FarmToFork() {
   return (
     <div className="farm_to_fork_landing">
       <Container>
-        <div className="top_content">Top Content</div>
-        <div className="center_contents">
-          <div className="left_contents">Left Content</div>
-          <div className="center_center">Center Content</div>
-          <div className="right_contents">Right Content</div>
-        </div>
-        <div className="bottom_contents">Bottom Content</div>
+        
       </Container>
     </div>
   );
