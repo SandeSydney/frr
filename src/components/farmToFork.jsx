ff62d4d6e7d2245b4cfd47b2dbebd7f0bce14407
SandeSydney <sandesydney01@gmail.com>
2024-07-28 14:37:01 +0300

    Structuring farm to fork section

--- src/components/farmToFork.jsx
+++ src/components/farmToFork.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function FarmToFork() {
-  return <div>FarmToFork</div>;
+  return (
+    <div className="farm_to_fork_landing">
+      <Container>
+        <div className="top_content">Top Content</div>
+        <div className="center_contents">
+          <div className="left_contents">Left Content</div>
+          <div className="center_center">Center Content</div>
+          <div className="right_contents">Right Content</div>
+        </div>
+        <div className="bottom_contents">Bottom Content</div>
+      </Container>
+    </div>
+  );
 }
 
 export default FarmToFork;
