ea002f5410809209a83c7d282074a3ad8f9e0a6b
SandeSydney <sandesydney01@gmail.com>
2024-07-29 09:38:32 +0300

    Added images to carousel

--- src/components/homeCarousel.jsx
+++ src/components/homeCarousel.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import cabbana from "../resources/mainGallery/cabbanaImage.webp";
-import horseBack from "../resources/mainGallery/horseBackLg.webp";
-import swimmingNight from "../resources/mainGallery/swimmingNightLg.webp"
+import farm from "../resources/mainGallery/farmLarge.webp";
+import swimmingNight from "../resources/mainGallery/swimmingNightLg.webp";
+import cowsBack from "../resources/mainGallery/cowsBack.webp";
 
 function HomeCarousel() {
   return (
@@ -11,10 +12,13 @@ function HomeCarousel() {
         <img className="d-block w-100" src={cabbana} alt="" />
       </Carousel.Item>
       <Carousel.Item interval={800}>
-        <img className="d-block w-100" src={horseBack} alt="" />
+        <img className="d-block w-100" src={farm} alt="" />
       </Carousel.Item>
       <Carousel.Item interval={800}>
         <img className="d-block w-100" src={swimmingNight} alt="" />
+      </Carousel.Item>
+      <Carousel.Item interval={800}>
+        <img className="d-block w-100" src={cowsBack} alt="" />
       </Carousel.Item>
     </Carousel>
   );
