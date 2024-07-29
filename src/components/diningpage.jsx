2667cbe391e023f1e4ebfc26ca54e53e8f505fff
SandeSydney <sandesydney01@gmail.com>
2024-07-29 16:43:49 +0300

    Added organic dining section

--- src/components/diningpage.jsx
+++ src/components/diningpage.jsx
@@ -21,7 +21,7 @@ export default function DiningPage() {
   // useInView to animate items on view
   const [landingRef, inLandingView] = useInView();
   const [chomaColRef, inChomaView] = useInView();
-  const [chomaRef, inChomaRefView] = useInView();
+  const [diningRef, inDiningRefView] = useInView();
 
   // useEffect in order to get a smooth scroll on mount of the dining page
   useEffect(() => {
@@ -63,7 +63,7 @@ export default function DiningPage() {
                   }
                 >
                   <ScrollLink
-                    to="choma_col"
+                    to="dining_text"
                     spy={true}
                     smooth={true}
                     offset={-70}
@@ -77,38 +77,54 @@ export default function DiningPage() {
             </div>
           </div>
         </div>
+        <div className="fresh_dining">
+          <Container
+            ref={diningRef}
+            className={inDiningRefView ? "elementShow" : "elementHide"}
+          >
+            <h2 id="dining_text">Organic Farm-Fresh Meals</h2>
+            <div>
+              <p
+                className={
+                  inDiningRefView ? "elementShowSlower" : "elementHide"
+                }
+              >
+                Welcome to the home of wholesome dining on{" "}
+                <b>Organic Farm-Fresh Produce</b>. Fred's Ranch and Resort is
+                nested within a 10-acre Organic Farm. At least 60% of the meals
+                we serve comes directly from the farm to your plates.
+              </p>
+              <p
+                className={
+                  inDiningRefView ? "elementShowSlower" : "elementHide"
+                }
+              >
+                This is a result of our commitment to high value, quality, and
+                sustainability. At Fred's Ranch and Resort, we give you an
+                experience of superior nutrition and vibrant flavors, just
+                cultivated close to where you dine.
+              </p>
+            </div>
+          </Container>
+        </div>
         <Container>
           <div className="dining_choma">
-            <Row>
-              <Col xs={12} sm={12} md={1}></Col>
-              <Col xs={12} sm={12} md={5} className="choma_col" id="choma_col">
-                <div
-                  ref={chomaColRef}
-                  className={inChomaView ? "elementShow" : "elementHide"}
-                >
-                  <h2>Fred's Ranch Mbuzi Choma.</h2>
-                  <p>
-                    Enjoy our sumptuous mbuzi choma as you celebate your events
-                    or special occassions, such as a weekend family
-                    get-together. It comes with a side of the famous staple food
-                    ugali and to top it with an alluring fresh vegetable salad
-                    (kachumbari). It doesn't get any better than this.
-                  </p>
-                  <img src={nyama_choma} alt="" />
-                </div>
-              </Col>
-              <Col xs={12} sm={12} md={5} className="my-3">
-                <div
-                  ref={chomaRef}
-                  className={
-                    inChomaRefView ? "elementShowSlower" : "elementHide"
-                  }
-                >
-                  <img className="nyama_img" src={nyama_choma2} alt="" />
-                </div>
-              </Col>
-              <Col xs={12} sm={12} md={1}></Col>
-            </Row>
+            <div>
+              <img src={nyama_choma} alt="" />
+            </div>
+            <div
+              ref={chomaColRef}
+              className={inChomaView ? "elementShow" : "elementHide"}
+            >
+              <h2>Fred's Ranch Mbuzi Choma.</h2>
+              <p>
+                Enjoy our sumptuous mbuzi choma as you celebate your events or
+                special occassions, such as a weekend family get-together. It
+                comes with a side of the famous staple food ugali and to top it
+                with an alluring fresh vegetable salad. It doesn't get any
+                better than this.
+              </p>
+            </div>
           </div>
         </Container>
         <div className="dining_menu">
