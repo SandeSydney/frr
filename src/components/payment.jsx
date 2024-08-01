73b06c55da3649663073bc037f436781b5354bfa
SandeSydney <sandesydney01@gmail.com>
2024-08-01 12:24:38 +0300

    Removed slower transitions

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -24,7 +24,7 @@ function PaymentMethods() {
           >
             Payment Methods Accepted
           </h4>
-          <div className={inViewPay ? "elementShowSlower" : "elementHide"}>
+          <div className={inViewPay ? "elementShow" : "elementHide"}>
             <img
               src={equitybank}
               alt="Equity Payment method in Fred's Ranch and Resort"
