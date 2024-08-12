a5e00bc3b871082e01884de8655b0e5aae67397d
SandeSydney <sandesydney01@gmail.com>
2024-08-12 15:36:33 +0300

    Value collection

--- src/components/contact_form.jsx
+++ src/components/contact_form.jsx
@@ -1,34 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [values, setValues] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    selectItem: "",
+    description: "",
+  });
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <>
       <div className="form_div">
-        <form action="" className="glass-form">
+        <form action="" onSubmit={handleSubmit}>
           <div className="control_main">
             <div className="form_control">
               <label htmlFor="firstname">First Name*:</label>
-              <input type="text" name="firstname" required />
+              <input
+                type="text"
+                name="firstname"
+                required
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
+              />
             </div>
             <div className="form_control">
               <label htmlFor="lastname">Last Name*:</label>
-              <input type="text" name="lastname" required />
+              <input
+                type="text"
+                name="lastname"
+                required
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
+              />
             </div>
           </div>
           <div className="control_main">
             <div className="form_control">
               <label htmlFor="email">Email*:</label>
-              <input type="email" name="email" id="email" required />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
+              />
             </div>
             <div className="form_control">
               <label htmlFor="phone">Phone Number*:</label>
-              <input type="tel" name="phone" id="phone" required />
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                required
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
+              />
             </div>
           </div>
           <div>
             <div className="form_control">
               <label htmlFor="selectItem">Which area can we be of help?</label>
-              <select name="selectItem" id="selectItem">
+              <select
+                name="selectItem"
+                id="selectItem"
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
+              >
                 <option value="reservation">Reservation</option>
                 <option value="restaurant">Restaurant</option>
                 <option value="accommodation">Accommodation</option>
@@ -44,6 +98,9 @@ function ContactForm() {
                 id="description"
                 placeholder="Type here..."
                 rows={4}
+                onChange={(e) => {
+                  handleChanges(e);
+                }}
               ></textarea>
             </div>
           </div>
