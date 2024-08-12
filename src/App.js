f8454729d7c39fe3b1a252478bb50dd07ea6c1ac
SandeSydney <sandesydney01@gmail.com>
2024-08-12 17:32:41 +0300

    Added Firebase Firestore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./firebaseConfig";
 import Footer from "./components/footer";
 import Homepage from "./components/homepage";
 import Navbar_Main from "./components/navbar";
