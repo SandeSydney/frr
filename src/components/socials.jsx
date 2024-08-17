62451dbbd84caf26c9d3d87a3c4f27b457d7710f
SandeSydney <sandesydney01@gmail.com>
2024-08-17 16:52:16 +0300

    Social Link updates

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -1,43 +1,45 @@
 import React from "react";
-import Instagram from "../resources/logos/instagramLogo.png";
-import Twitter from "../resources/logos/twitterLogo.png";
-import Facebook from "../resources/logos/facebookLogo.png";
-import Tiktok from "../resources/logos/tiktokLogo.png";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Link as RouterLink } from "react-router-dom";
+import Facebook from "../resources/logos/facebookLogo.png";
+import Instagram from "../resources/logos/instagramLogo.png";
+import Tiktok from "../resources/logos/tiktokLogo.png";
+import Twitter from "../resources/logos/twitterLogo.png";
 
 function Socials() {
-  const returnLink = "https://frr-v1.vercel.app/";
-
   return (
     <div className="socials">
       <h4>Follow Us On:</h4>
       <Row>
         <Col className="social_cols" xs={12}>
           <RouterLink
-            to={`/redirect?targetLink=https://www.instagram.com/freds_ranch/&returnLink=${returnLink}`}
+            to="https://www.instagram.com/freds_ranch"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={Instagram} alt="Fred's Ranch Instagram Page" /> Freds
             Ranch
           </RouterLink>
         </Col>
         <Col className="social_cols" xs={12}>
-          <RouterLink
-            to={`/redirect?targetLink=https://x.com/freds_ranch/&returnLink=${returnLink}`}
-          >
+          <RouterLink to="">
             <img src={Twitter} alt="Fred's Ranch Twitter Page" /> freds_ranch
           </RouterLink>
         </Col>
         <Col className="social_cols" xs={12}>
           <RouterLink
-            to={`/redirect?targetLink=https://www.facebook.com/fredsranch/&returnLink=${returnLink}`}
+            to="https://www.facebook.com/fredsranch"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={Facebook} alt="Fred's Ranch Facebook Page" /> fredsranch
           </RouterLink>
         </Col>
         <Col className="social_cols" xs={12}>
           <RouterLink
-            to={`/redirect?targetLink=https://www.tiktok.com/@freds_ranch/&returnLink=${returnLink}`}
+            to="https://www.tiktok.com/@freds_ranch"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={Tiktok} alt="Fred'd Ranch Tiktok Page" /> @freds_ranch
           </RouterLink>
