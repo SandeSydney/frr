17e643552b94db33e183e1de494b53db9047eb18
SandeSydney <sandesydney01@gmail.com>
2024-08-17 14:59:59 +0300

    Updates with dp

--- src/components/whatsappCta.jsx
+++ src/components/whatsappCta.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { FloatingWhatsApp } from "@carlos8a/react-whatsapp-floating-button";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import logo from "../resources/mainGallery/frontOffice.webp";
 
 function WhatsappCta() {
   return (
     <div>
       <FloatingWhatsApp
-        phoneNumber="254792564243" // Required
-        accountName="Fred's Ranch and Resort" // Optional
-        // avatar="../" // Optional
-        initialMessageByServer="Hi there! How can I assist you?" // Optional
-        statusMessage="Available" // Optional
-        placeholder="Write here..." // Optional
-        allowEsc={true} // Optional
-        // Explore all available props below
+        phoneNumber="254792564243"
+        accountName="Fred's Ranch and Resort"
+        avatar={logo}
+        chatMessage="Hi there! How can I assist you today?"
+        statusMessage="Typically replies within 10 min"
+        placeholder="Write here..."
+        allowEsc={true}
       />
     </div>
   );
