787f39eb1aa365b05f7dcba8be6d9dba6c65aa47
SandeSydney <sandesydney01@gmail.com>
2024-08-18 19:58:14 +0300

    Socials update

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { Link as RouterLink } from "react-router-dom";
 import Facebook from "../resources/logos/facebookLogo.png";
 import Instagram from "../resources/logos/instagramLogo.png";
 import Tiktok from "../resources/logos/tiktokLogo.png";
@@ -12,60 +11,60 @@ function Socials() {
       <h4>Follow Us On:</h4>
       <Row>
         <Col className="social_cols" xs={12}>
-          <RouterLink
-            to="https://www.instagram.com/freds_ranch"
+          <img
+            src={Instagram}
+            alt="Fred's Ranch Instagram Page"
+            title="Fred's Ranch Instagram"
+            loading="lazy"
+          />
+          <a
+            href="https://www.instagram.com/freds_ranch"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src={Instagram}
-              alt="Fred's Ranch Instagram Page"
-              title="Fred's Ranch Instagram"
-              loading="lazy"
-            />{" "}
             Freds Ranch
-          </RouterLink>
+          </a>
         </Col>
         <Col className="social_cols" xs={12}>
-          <RouterLink to="">
-            <img
-              src={Twitter}
-              alt="Fred's Ranch Twitter Page"
-              title="Fred's Ranch Twitter Handle"
-              loading="lazy"
-            />{" "}
+          <img
+            src={Twitter}
+            alt="Fred's Ranch Twitter Page"
+            title="Fred's Ranch Twitter Handle"
+            loading="lazy"
+          />
+          <a href="" target="_blank" rel="noopener noreferrer">
             freds_ranch
-          </RouterLink>
+          </a>
         </Col>
         <Col className="social_cols" xs={12}>
-          <RouterLink
-            to="https://www.facebook.com/fredsranch"
+          <img
+            src={Facebook}
+            alt="Fred's Ranch Facebook Page"
+            title="Fred's Ranch Facebook Handle"
+            loading="lazy"
+          />
+          <a
+            href="https://www.facebook.com/fredsranch"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src={Facebook}
-              alt="Fred's Ranch Facebook Page"
-              title="Fred's Ranch Facebook Handle"
-              loading="lazy"
-            />{" "}
-            fredsranch
-          </RouterLink>
+            Freds Ranch
+          </a>
         </Col>
         <Col className="social_cols" xs={12}>
-          <RouterLink
-            to="https://www.tiktok.com/@freds_ranch"
+          <img
+            src={Tiktok}
+            alt="Fred'd Ranch Tiktok Page"
+            title="Fred's Ranch Tiktok Handle"
+            loading="lazy"
+          />
+          <a
+            href="https://www.tiktok.com/@freds_ranch"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src={Tiktok}
-              alt="Fred'd Ranch Tiktok Page"
-              title="Fred's Ranch Tiktok Handle"
-              loading="lazy"
-            />{" "}
-            @freds_ranch
-          </RouterLink>
+            Freds Ranch
+          </a>
         </Col>
       </Row>
     </div>
