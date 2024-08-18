5a1cffa44d7f789d53c98fa1c32c86d63e930a31
SandeSydney <sandesydney01@gmail.com>
2024-08-18 19:39:59 +0300

    Structural changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import "./firebaseConfig";
 import Footer from "./components/footer";
-import Homepage from "./components/homepage";
-import Navbar_Main from "./components/navbar";
-import Navbar_Sec from "./components/navbar_sec";
+import NAVBAR_MAIN from "./components/navbar";
+import NAVBAR_SEC from "./components/navbar_sec";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import WhatsappCta from "./components/whatsappCta";
 import "react-toastify/dist/ReactToastify.css";
@@ -23,7 +22,7 @@ function App() {
           setVisible(false);
         }
       })}
-      {visible ? <Navbar_Main /> : <Navbar_Sec />}
+      {visible ? <NAVBAR_MAIN/> : <NAVBAR_SEC />}
       <Outlet />
       <Footer />
       <WhatsappCta />
