db8e6ac95599c8cdabb3a2c10e08d89b281b72d7
SandeSydney <sandesydney01@gmail.com>
2024-11-29 16:57:55 +0300

    gold element show when in view

--- src/components/goldvip.jsx
+++ src/components/goldvip.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useInView } from "react-intersection-observer";
 
 function GoldVIP() {
+  // Check to see if component is scrolled into view
+  const [goldRef, inViewGold] = useInView();
+
   return (
     <div className="gold-main">
       <Container className="vip-container">
         <div className="goldvip">
-          <h2>Gold VIP Membership</h2>
-          <p>
+          <h2
+            ref={goldRef}
+            className={inViewGold ? "elementShow" : "elementHide"}
+          >
+            Gold VIP Membership
+          </h2>
+          <p
+            ref={goldRef}
+            className={inViewGold ? "elementShowSlower" : "elementHide"}
+          >
             Get to experience distinguished service only at Fred's Ranch &
             Resort. The secret is by subscribing to the VIP Membership
             Programme. Fred's Ranch & Resort showers it's registered loyal
@@ -15,7 +27,11 @@ function GoldVIP() {
             amenities as the VIP Cabana, Swimming Pool, Organic Farm, and Horse
             Rides, among others.
           </p>
-          <a href="https://gold-loyalty.fredsranch.co.ke">
+          <a
+            ref={goldRef}
+            className={inViewGold ? "elementShowSlower" : "elementHide"}
+            href="https://gold-loyalty.fredsranch.co.ke"
+          >
             <button>Check it out</button>
           </a>
         </div>
