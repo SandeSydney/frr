649b72a4ece0a5ed63bf78f6481bec193185b5ae
SandeSydney <sandesydney01@gmail.com>
2024-11-30 13:29:28 +0300

    Added image attributes

--- src/components/adsCarousel.jsx
+++ src/components/adsCarousel.jsx
@@ -7,7 +7,7 @@ function AdsCarousel() {
     <div className="ads-carousel-main">
       <div className="ads-carousel-top"></div>
       <div className="ads-carousel-content">
-      <Image src={rhumba_rockout} fluid />;
+      <Image alt="Rhumba Fred's Ranch 12th December" title="Rhumba Fred's Ranch 12th December"  loading="lazy" src={rhumba_rockout} fluid />;
         {/* <img src={rhumba_rockout} alt="" /> */}
       </div>
     </div>
