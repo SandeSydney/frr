a68cbe7f3d16d3ff8079a2361cbbe7b57bf95448
SandeSydney <sandesydney01@gmail.com>
2025-01-18 09:59:32 +0300

    Created banner

--- src/components/bannerCarousel.js
+++ src/components/bannerCarousel.js
@@ -3,7 +3,16 @@ import { Container, Image } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import grandma_xmas from "../resources/posters/Cowboy Meets Grandma Package.webp";
+import ikutoke_poster from "../resources/posters/Ikutoke poster.webp";
 import FestiveMenuModal from "./festiveMenuModal";
+import cabbana from "../resources/mainGallery/cabbanaImage.webp";
+import farm from "../resources/mainGallery/farmLarge.webp";
+import swimmingNight from "../resources/mainGallery/swimmingNightLg.webp";
+import cowsBack from "../resources/mainGallery/cowsBack.webp";
+import uncleCows from "../resources/mainGallery/uncleCows.webp";
+import divingLg from "../resources/mainGallery/divingLg.webp";
+import teamBuildingLg from "../resources/mainGallery/teamBuildingLg.webp";
+import chomaRibsLg from "../resources/mainGallery/chomaRibsLg.webp";
 
 function BannerCarousel() {
   const [modalShow, setModalShow] = React.useState(false);
@@ -18,7 +27,7 @@ function BannerCarousel() {
         max: 3000,
         min: 1024,
       },
-      items: 1,
+      items: 2,
       partialVisibilityGutter: 40,
     },
     mobile: {
@@ -41,7 +50,7 @@ function BannerCarousel() {
 
   return (
     <Container>
-      <FestiveMenuModal show={modalShow} onHide={() => setModalShow(false)} />
+      {/* <FestiveMenuModal show={modalShow} onHide={() => setModalShow(false)} /> */}
       <div className="banner-carousel-main">
         <div className="carousel-div">
           <Carousel
@@ -70,26 +79,56 @@ function BannerCarousel() {
             shouldResetAutoplay
             showDots={false}
             sliderClass=""
-            slidesToSlide
+            slidesToSlide={2}
             swipeable
+            ssr={true}
           >
             <Image
-              alt="Christmas Fred's Ranch 25th December"
-              title="Christmas Fred's Ranch 25th December"
+              src={cabbana}
+              alt="Cabbana Image"
+              title="Fred's Ranch and Resort"
               loading="eager"
-              src={grandma_xmas}
-              fluid
+            />
+            <Image
+              src={farm}
+              alt="Farm image"
+              title="Fred's Ranch and Resort Farm tour"
+              loading="eager"
+            />
+            <Image
+              src={teamBuildingLg}
+              alt="Fred's Ranch Cows from the back"
+              title="Fred's Ranch and Resort Team Building"
+              loading="eager"
+            />
+            <Image
+              src={chomaRibsLg}
+              alt="Farm image"
+              title="Fred's Ranch and Resort Nyama Choma"
+              loading="eager"
+            />
+            <Image
+              src={divingLg}
+              alt="Fred's Ranch Cows from the back"
+              title="Fred's Ranch and Resort Diving"
+              loading="eager"
+            />
+            <Image
+              src={cowsBack}
+              alt="Fred's Ranch Cows from the back"
+              title="Fred's Ranch and Resort Dairy cows back"
+              loading="eager"
             />
           </Carousel>
         </div>
-        <div className="carousel-left">
+        {/* <div className="carousel-left">
           <button
             className="festive-menu-btn"
             onClick={() => setModalShow(true)}
           >
             Fred's Ranch Festive Menu
           </button>
-        </div>
+        </div> */}
       </div>
     </Container>
   );
